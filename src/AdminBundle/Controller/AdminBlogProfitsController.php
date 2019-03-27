1104e639d3a7c0a977a965927a3d4bd733c9ee97
Achir Adel Mounir <47035700+AchirAdelMounir@users.noreply.github.com>
2019-03-27 02:11:05 +0100

    Profits

---
+++ src/AdminBundle/Controller/AdminBlogProfitsController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace BlogBundle\Controller;
+
+use AppBundle\Entity\Blogposts;
+use AppBundle\Entity\BlogProfits;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class AdminBlogProfitsController extends Controller
+{
+    public function AddEntryAction()
+    {    $em=$this->getDoctrine()->getManager();
+$date=new \DateTime();
+        $datefin=date_add($date, date_interval_create_from_date_string('30 days'));
+
+
+        $user=$this->container->get('security.token_storage')->getToken()->getUser();
+        $profit=new BlogProfits();
+        $likes=$em->getRepository(Blogposts::class)->CountAllLikes($user->getId());
+        $comments=$em->getRepository(Blogposts::class)->CountAllComs($user->getId());
+
+        $profit->setPostLikesCount(intval(reset($likes)));
+        $profit->setPostCommentCount(intval(reset($comments)));
+        $profit->setAuthor($user);
+        $profit->setStartDate(new \DateTime());
+        $profit->setEndDate($datefin);
+
+        $profit->setStatus(1);
+
+
+        $em->persist($profit);
+        $em->flush();
+        return $this->redirectToRoute('blog_Managment');
+
+
+    }
+    public function listPaymentsAction()
+    {
+
+    }
+}
