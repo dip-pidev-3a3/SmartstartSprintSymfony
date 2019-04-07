72cf299360515a08da48a7818d48e2c7a38069fd
larou2si <mohamed.laroussi.1@esprit.tn>
2019-04-07 12:33:11 +0100

    larou

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -27,6 +27,9 @@ class AppKernel extends Kernel
             new Knp\Bundle\SnappyBundle\KnpSnappyBundle(),
             new ComplaintsBundle\ComplaintsBundle(),
             new FeedbackBundle\FeedbackBundle(),
+            new QandABundle\QandABundle(),
+            new FOS\CKEditorBundle\FOSCKEditorBundle(),
+            new Knp\Bundle\MenuBundle\KnpMenuBundle(),
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
