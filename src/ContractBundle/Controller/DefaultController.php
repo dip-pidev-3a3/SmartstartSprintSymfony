88f0029a9e7c0ae1c8fe40e97704cc1a8769c1da
MohamedAmine1 <mohamedamine.lamari@esprit.tn>
2019-04-07 15:30:37 +0100

    hello chatti

--- src/ContractBundle/Controller/DefaultController.php
+++ src/ContractBundle/Controller/DefaultController.php
@@ -8,17 +8,36 @@ use AppBundle\Entity\FosUser;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\HiddenType;
+use Symfony\Component\Validator\Constraints\NotBlank;
 
 
 
 class DefaultController extends Controller
 {
-    public function indexAction()
+    public function indexAction(Request $request)
     {
+        $form = $this->get('form.factory')
+            ->createNamedBuilder('payment-form')
+            ->add('token', HiddenType::class, [
+                'constraints' => [new NotBlank()],
+            ])
+            ->getForm();
+
+        if ($request->isMethod('POST')) {
+            $form->handleRequest($request);
+
+            if ($form->isValid()) {
+                // TODO: charge the card
+
+            }
+        }
         $user=new FosUser();
         $user = $this->container->get('security.token_storage')->getToken()->getUser();
         $contracts = $this->getDoctrine()->getRepository(Contract::class)->getMyContracts($user->getId());
-        return $this->render('@Contract/Default/index.html.twig',array('con'=>$contracts));
+        return $this->render('@Contract/Default/index.html.twig',array('con'=>$contracts,'form' => $form->createView(),
+            'stripe_public_key' => "pk_test_RzueYgRk554ZpsUNBPruZ42P"));
     }
     public function deleteAction($id){
         $em = $this->getDoctrine()->getManager();
